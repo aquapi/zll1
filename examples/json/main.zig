afbd6f4854c5d0b4546226522dbec4ea425bbd98
aquapi <aquaplmc@gmail.com>
2026-06-04 21:30:49 +0700

    update

--- examples/json/main.zig
+++ examples/json/main.zig
@@ -3,23 +3,20 @@ const parser = @import("zll1").parser;
 
 const grammar = @import("./grammar.zig");
 
-pub fn main() void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.smp_allocator);
-    defer arena.deinit();
+pub fn main(init: std.process.Init) void {
+    const arena = init.arena.allocator();
 
-    const allocator = arena.allocator();
-
-    const example_json = parser.parse(grammar.JSON, allocator,
+    const example_json = parser.parse(grammar.JSON, arena,
         \\{
         \\  "hello": "world",
         \\  "id": 0
         \\}
     ).?;
-    defer parser.deparse(grammar.JSON, allocator, example_json);
+    defer parser.deparse(grammar.JSON, arena, example_json);
 
     {
-      const props = example_json.object[1].?;
-      _ = props[0][2].cast().string;
-      _ = props[1].items[0][1][2].cast().number;
+        const props = example_json.object[1].?;
+        _ = props[0][2].cast().string;
+        _ = props[1].items[0][1][2].cast().number;
     }
 }
