e7da16ebc79d9b80c283b6e5244f3455cc4f8d38
aquapi <aquaplmc@gmail.com>
2026-06-04 21:38:58 +0700

    update

--- examples/json/main.zig
+++ examples/json/main.zig
@@ -6,15 +6,15 @@ const grammar = @import("./grammar.zig");
 pub fn main(init: std.process.Init) void {
     const arena = init.arena.allocator();
 
-    const example_json = parser.parse(grammar.JSON, arena,
-        \\{
-        \\  "hello": "world",
-        \\  "id": 0
-        \\}
-    ).?;
-    defer parser.deparse(grammar.JSON, arena, example_json);
-
     {
+        const example_json = parser.parse(grammar.JSON, arena,
+            \\{
+            \\  "hello": "world",
+            \\  "id": 0
+            \\}
+        ).?;
+        defer parser.deparse(grammar.JSON, arena, example_json);
+
         const props = example_json.object[1].?;
         _ = props[0][2].cast().string;
         _ = props[1].items[0][1][2].cast().number;
