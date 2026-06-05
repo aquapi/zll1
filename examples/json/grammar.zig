3fc5cb84748e81a2a1c3de530af8366ecdb21ee9
aquapi <aquaplmc@gmail.com>
2026-06-05 16:15:57 +0700

    hmm

--- examples/json/grammar.zig
+++ examples/json/grammar.zig
@@ -1,19 +1,15 @@
 const p = @import("zll1").parser;
 
-pub const Value = p.Union(.{ .number = Number, .string = String, .nil = p.Const("null"), .true = p.Const("true"), .false = p.Const("false"), .object = Object, .array = Array });
+pub const Value = p.Recursive(struct {
+    pub fn init() type {
+        return p.Union(.{ .number = Number, .string = String, .nil = p.Const("null"), .true = p.Const("true"), .false = p.Const("false"), .object = Object, .array = Array });
+    }
+});
 
 pub const Number = p.Float;
 pub const String = p.String('"');
 
-const Pair = p.Recursive(struct {
-    pub fn init() type {
-        return p.Tuple(.{ String, p.Prefix(":", p.Ref(Value)) });
-    }
-});
+const Pair = p.Tuple(.{ String, p.Prefix(":", p.Ref(Value)) });
 pub const Object = p.Wrap("{", p.Array(Pair, ","), "}");
 
-pub const Array = p.Recursive(struct {
-    pub fn init() type {
-        return p.Wrap("[", p.Array(p.Ref(Value), ","), "]");
-    }
-});
+pub const Array = p.Wrap("[", p.Array(p.Ref(Value), ","), "]");
